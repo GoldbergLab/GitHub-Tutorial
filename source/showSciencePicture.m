2b4a1fd4551058b3143a714814bac27265e41eb0
Brian Kardon <brian.kardon@gmail.com>
2021-04-15 12:27:06 -0400

    Changed text to lower case

--- source/showSciencePicture.m
+++ source/showSciencePicture.m
@@ -1,4 +1,4 @@
 image = imread(imagePath);
 f = figure;
 imshow(imrotate(imresize(image, 0.25), -90));
-text(10, 20, 'YAY YOU USED GIT TO GET SOFTWARE!', 'FontSize', 18, 'Color', [0.8500 0.3250 0.0980]);
+text(10, 20, 'Yay, you used Git and GitHub to get software!', 'FontSize', 18, 'Color', [0.8500 0.3250 0.0980]);
